eff4b61f0401334c5d06f7e8ccd60c08cc6a43fa
Heller <hefa_25@hotmail.com>
2025-01-06 09:17:58 -0500

    fix version

--- app/src/main/java/com/hlabs/paguelofacildemo/data/AppRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.hlabs.paguelofacildemo.data
-
-import com.hlabs.paguelofacildemo.api.ApiResultData
-import com.hlabs.paguelofacildemo.domain.model.PagueloFacilResponse
-
-interface AppRepository {
-
-    suspend fun getMovements(s: String,filter:String,date:String): ApiResultData<PagueloFacilResponse>
-}
